4b3b4fd254e3c92aa42a9f892ba4884aa34fda40
Claudio Gomes da Silva Neto <claudiogsn2@gmail.com>
2024-09-30 03:56:44 -0300

    final adjustments

--- balanco.php
+++ balanco.php
@@ -23,7 +23,7 @@ if (isset($_GET['tag'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Redirecionamento de Balanço</title>
+    <title>Realziar Balanço</title>
     <style>
         /* O iframe ocupará toda a tela */
         html, body {
