65738d6f2cccdbd37782052c91ea65359c927297
Claudio Gomes da Silva Neto <claudiogsn2@gmail.com>
2024-11-04 19:11:45 -0300

    create transfrencia assitidas

---
+++ app/control/TransferenciaAssitida.class.php
@@ -0,0 +1,36 @@
+<?php
+class TransferenciaAssitida extends TPage
+{
+    private $form;
+    public function __construct($param)
+    {
+        parent::__construct();
+
+        $username = TSession::getValue('userid');
+        $token = TSession::getValue('sessionid');
+        $unit_id = TSession::getValue('userunitid');
+
+
+        if($_SERVER['SERVER_NAME'] == "localhost"){
+            $link = "http://localhost/portal-mrk/external/transferenciaAssistida.html?username={$username}&token={$token}&unit_matriz_id=8";
+        }else{
+            $link = "https://portal.mrksolucoes.com.br/external/external/transferenciaAssistida.html?username={$username}&token={$token}&unit_matriz_id=8";
+        }
+
+        $iframe = new TElement('iframe');
+        $iframe->id = "iframe_external";
+        $iframe->src = $link;
+        $iframe->frameborder = "0";
+        $iframe->scrolling = "yes";
+        $iframe->width = "100%";
+        $iframe->height = "800px";
+
+        parent::add($iframe);
+    }
+    function onFeed($param){
+        // $id = $param['key'];
+    }
+    function onEdit($param){
+        // $id = $param['key'];
+    }
+}
