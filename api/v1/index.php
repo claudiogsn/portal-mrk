b3cfa61122d96fbc8274cf44a782a52d82216af3
Claudio Gomes da Silva Neto <claudiogsn2@gmail.com>
2025-05-14 22:50:55 -0300

    no auth methos

--- api/v1/index.php
+++ api/v1/index.php
@@ -47,7 +47,7 @@ if (isset($data['method']) && isset($data['data'])) {
     if (isset($data['token'])){$requestToken = $data['token'];}
 
     // Métodos que não precisam de autenticação
-    $noAuthMethods = ['validateCPF', 'validateCNPJ','getModelByTag','saveBalanceItems','getUnitsByGroup','registerJobExecution','persistSales','consolidateSalesByGroup','importMovBySalesCons'];
+    $noAuthMethods = ['validateCPF', 'persistMovimentoCaixa','validateCNPJ','getModelByTag','saveBalanceItems','getUnitsByGroup','registerJobExecution','persistSales','consolidateSalesByGroup','importMovBySalesCons'];
 
     if (!in_array($method, $noAuthMethods)) {
         if (!isset($requestToken)) {
